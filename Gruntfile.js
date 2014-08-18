4c874f4259388d77f42735df96a5d9b3c7479c85
Bruno Almeida <bruno@disvolvi.com>
2014-08-18 08:35:08 -0300

    Add notify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,36 @@
 module.exports = function(grunt) {
 
 	// Load all tasks
+	grunt.loadNpmTasks('grunt-notify');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 
 	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
-		// Concat and minify javascripts
-		uglify: {
-			options: {
-				mangle: false
-			},
-			dist: {
-				files: {
-					'./js/application.js': [
-					'./src/js/*.js'
-					]
-				}
-			} 
+		notify: {
+
+			// Concat and minify javascripts
+			uglify: {
+				options: {
+					mangle: false
+				},
+				dist: {
+					files: {
+						'./js/application.js': [
+						'./src/js/*.js'
+						]
+					}
+				},
+				options: {
+        			title: 'Uglify Taks',
+        			message: 'Uglify task completed successful',
+      			}
+			}
 		}
+
 	});
 
 	// registering task default
-	grunt.registerTask( 'default', [ 'uglify' ] );
+	grunt.registerTask( 'default', [ 'notify:uglify' ] );
 };
