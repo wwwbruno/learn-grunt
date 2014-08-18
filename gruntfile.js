eb35fb2452b915c5543118559d605671c93242e0
Bruno Almeida <bruno@disvolvi.com>
2014-08-18 10:03:06 -0300

    Minify images (without notifying this yet)

--- gruntfile.js
+++ gruntfile.js
@@ -3,6 +3,7 @@ module.exports = function(grunt) {
 	// Load all tasks
 	grunt.loadNpmTasks('grunt-notify');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
 
 	// Project configuration.
 	grunt.initConfig({
@@ -26,11 +27,30 @@ module.exports = function(grunt) {
         			title: 'Uglify Task',
         			message: 'Uglify task completed successful',
       			}
-			}
+			},
+		},
+
+		// Optimize images
+		imagemin: {
+			dynamic: {
+				options: {
+					optimizationLevel: 3,
+				},
+				files: [{
+					expand: true,
+					cwd: 'src/images/',
+					src: ['*.{png,jpg,gif}'],
+					dest: 'images/'
+				}]
+			},
+			options: {
+    			title: 'Imagemin Task',
+    			message: 'Imagemin task completed successful',
+  			}
 		}
 
 	});
 
 	// registering task default
-	grunt.registerTask( 'default', [ 'notify:uglify' ] );
+	grunt.registerTask( 'default', [ 'notify:uglify', 'imagemin' ] );
 };
